e2132101b8447c51a66e2a148755996592e04aa2
Virkkunen <vrkknn@vrkknn.net>
2023-01-11 19:34:19 -0300

    req 05 gravatar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import md5 from 'crypto-js/md5';
 
-export default class Header extends Component {
+class Header extends Component {
+  getAvatar = () => {
+    const { email } = this.props;
+    const address = String(email).trim().toLowerCase();
+    const hash = md5(address);
+    return `https://www.gravatar.com/avatar/${hash}`;
+  };
+
   render() {
     return (
       <div>
         Header
+        <div>
+          <img
+            alt="user.name"
+            src={ this.getAvatar() }
+          />
+        </div>
       </div>
     );
   }
 }
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  // email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ...state.user,
+});
+
+export default connect(mapStateToProps)(Header);
