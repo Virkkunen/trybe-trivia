18d286fe5b2a6bffca89bdd10282393c4edca51d
Virkkunen <vrkknn@vrkknn.net>
2023-01-11 17:01:00 -0300

    req 03

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
-  handleClick = ({ target: { name } }) => {
-    const { history } = this.props;
-    history.push(`/${name}`);
-  };
+
 
   render() {
     return (
       <div>
         Header
-        <div>
-          <button
-            type="button"
-            data-testid="btn-settings"
-            name="settings"
-            onClick={ this.handleClick }
-          >
-            Configurações
-          </button>
-        </div>
       </div>
     );
   }
 }
-
-Header.propTypes = {
-  // dispatch: PropTypes.func.isRequired,
-  history: PropTypes.instanceOf(Object).isRequired,
-};
