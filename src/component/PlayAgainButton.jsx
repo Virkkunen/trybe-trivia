c4bd0b5f9872394516ada6428a68a4fa89e5d18e
Virkkunen <vrkknn@vrkknn.net>
2023-01-13 18:32:15 -0300

    req 18

--- src/component/PlayAgainButton.jsx
+++ src/component/PlayAgainButton.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default class PlayAgainButton extends Component {
   render() {
+    const { testId } = this.props;
+
     return (
       <Link to="/">
         <button
           type="button"
-          data-testid="btn-play-again"
+          data-testid={ testId }
         >
           Jogar novamente
         </button>
@@ -15,3 +18,7 @@ export default class PlayAgainButton extends Component {
     );
   }
 }
+
+PlayAgainButton.propTypes = {
+  testId: PropTypes.string.isRequired,
+};
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import PlayAgainButton from '../component/PlayAgainButton';
 
 export default class Ranking extends Component {
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <PlayAgainButton testId="btn-go-home" />
       </div>
     );
   }
